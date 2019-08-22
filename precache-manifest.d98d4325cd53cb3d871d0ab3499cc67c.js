ca944155456b05cf5ff351982d5162e4be7449c1
horlathunbhosun <olulodeolatunbosun@yahoo.com>
2019-08-22 10:53:48 +0100

    Updates

---
+++ precache-manifest.d98d4325cd53cb3d871d0ab3499cc67c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4060e41abd9ee388546a0ad05bc89d",
+    "url": "/react-searchfilter/index.html"
+  },
+  {
+    "revision": "933ec674a7a8207dabe1",
+    "url": "/react-searchfilter/static/css/main.1f425889.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react-searchfilter/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "933ec674a7a8207dabe1",
+    "url": "/react-searchfilter/static/js/main.c6444afe.chunk.js"
+  },
+  {
+    "revision": "68bba547942b7634eb80",
+    "url": "/react-searchfilter/static/js/runtime~main.99cdd615.js"
+  }
+]);
